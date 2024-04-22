61b417be60137922e95cdba6405ffaa630bd3447
Moyo Oyegunle <fake@example.com>
2024-04-21 23:24:27 -0400

    updates

--- customer-app/build.sh
+++ customer-app/build.sh
@@ -6,8 +6,8 @@ oc new-project $APP_NAMESPACE
 
 #Build Binary
 cd $foldername
-# ./mvnw package -Dnative -Dmaven.test.skip=true -Dquarkus.native.container-build=true
+./mvnw package -Dnative -Dmaven.test.skip=true -Dquarkus.native.container-build=true
 
-# oc new-build --binary=true --name=customer-app -n $APP_NAMESPACE
-# oc start-build customer-app --from-dir=. --follow -n $APP_NAMESPACE
+oc new-build --binary=true --name=customer-app -n $APP_NAMESPACE
+oc start-build customer-app --from-dir=. --follow -n $APP_NAMESPACE
 oc kustomize ./deploy/ | oc apply -f - -n $APP_NAMESPACE
